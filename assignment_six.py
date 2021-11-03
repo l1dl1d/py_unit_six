5f5d0fc4d1ea8b0f8389ca79f0c9a4be4e2a1b37
l1dl1d <javeekh@ssfs.org>
2021-11-03 14:31:08 -0400

    in the middle of assignment six

---
+++ assignment_six.py
@@ -0,0 +1,13 @@
+import random
+def get_birthdays():
+    list_one = []
+    for x in range(23):
+        r = random.randint(1, 365)
+        list_one.append(r)
+        return list_one
+def is_duplicates(list_one):
+    for x in range(22):
+        list_one[x]
+        for y in range(x+1, 23):
+            list_one[y]
+    if list_one[x] == [y]:
