909f314346b4cd66704fb785a1cb695c985e9afb
l1dl1d <javeekh@ssfs.org>
2021-11-09 09:29:47 -0500

    done assignmnet 6

--- assignment_six.py
+++ assignment_six.py
@@ -7,19 +7,17 @@ def get_birthdays():
     return list_one
 def is_duplicates(list_one):
     for x in range(22):
-
         for y in range(x+1, 23):
-
             if list_one[x] == list_one[y]:
                 return True
     return False
 def main():
-    runs = input("how many times would you like to simulate this problem?")
+    runs = input("How many times would you like to simulate this problem?")
     duplicate = 0
-    for x in range(runs):
+    for x in range(int(runs)):
         birthdays = get_birthdays()
         if is_duplicates(birthdays):
             duplicate = duplicate + 1
-
-    print()
-
+    print(duplicate, "times there were two of the same birthday. This is about", duplicate/int(runs), "percent of the time.")
+if __name__ == '__main__':
+    main()
